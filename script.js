47543097219d0f2c6648af8fd959ccee5501e0fd
Lacy <lacy.mroz91@gmail.com>
2022-11-07 17:28:34 -0500

    Prompt user for choice

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice(){
     let computerChoice=Math.random();//generates a random number between 1 and 0
     if (computerChoice > 0 && computerChoice < .333){ //defines random numbers into rock, paper, scissor choices
@@ -8,8 +10,12 @@ function getComputerChoice(){
         return 'scissors'
     }
 }
-console.log(getComputerChoice());
+let playerSelection = prompt('Rock, Paper, or Scissors?');
+let computerSelection=getComputerChoice();
 
-//Ask for player selection
+function playRound(playerSelection, computerSelection){//Ask for player selection
+
+}
+
 //Establish hierarchy of rock, paper, and scissors
 //Return 'You Win' vs. The Computer win based on that hierarchy.

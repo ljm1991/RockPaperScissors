fbd75b8168c3ab335eaff39e8984623b85e5a3c4
Lacy <lacy.mroz91@gmail.com>
2022-11-07 18:25:58 -0500

    Fix typos in getComputerChoice function

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ function getComputerChoice(){
     if (computerChoice > 0 && computerChoice < .333){ //defines random numbers into rock, paper, scissor choices
         return 'rock';
     } else if (computerChoice > .333 && computerChoice < .666){
-        return 'paper;'
+        return 'paper';
     } else {
-        return 'scissors'
+        return 'scissors';
     }
 }
 let playerSelection = prompt('Rock, Paper, or Scissors?'); //Ask for player selection
 let computerSelection=getComputerChoice();
 
 function playRound(playerSelection, computerSelection){
-    if (computerSelection=='rock' && playerSelection=='paper'){
+    if (computerSelection === playerSelection){
+        return 'Tie';
+    } else if (computerSelection=='rock' && playerSelection=='paper'){
         return 'You win';
     } else if (computerSelection == 'paper' && playerSelection=='scissors'){
         return 'You win';
@@ -25,15 +27,12 @@ function playRound(playerSelection, computerSelection){
     } else if (computerSelection=='paper' && playerSelection=='rock'){
         return 'Computer wins';
     } else if (computerSelection=='scissors' && playerSelection=='paper'){
-        return 'Computer wins';                                                                            
-    } else {
-        return 'Tie';
-    }
+        return 'Computer wins';                                                                                 
 }
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound());
-
+}
+console.log (computerSelection);
+console.log (playerSelection);
+console.log (playRound());
 
 //Establish hierarchy of rock, paper, and scissors
 //Return 'You Win' vs. The Computer win based on that hierarchy.

b0a87657be1e77a49af0f97511a188f923337694
Lacy <lacy.mroz91@gmail.com>
2022-11-07 19:55:13 -0500

    Add reveal of both choices in playRound returns

--- script.js
+++ script.js
@@ -13,26 +13,24 @@ function getComputerChoice(){
 let playerSelection = prompt('Rock, Paper, or Scissors?'); //Ask for player selection
 let computerSelection=getComputerChoice();
 
-function playRound(playerSelection, computerSelection){
+function playRound(computerSelection, playerSelection){ //Establish hierarchy of rock, paper, and scissors
     if (computerSelection === playerSelection){
         return 'Tie';
-    } else if (computerSelection=='rock' && playerSelection=='paper'){
-        return 'You win';
+    }
+    else if (computerSelection=='rock' && playerSelection=='paper'){
+        return 'You win! Paper beats rock!';
     } else if (computerSelection == 'paper' && playerSelection=='scissors'){
-        return 'You win';
+        return 'You win! Scissors beats paper!';
     } else if (computerSelection == 'scissors' && playerSelection=='rock'){
-        return 'You win';
+        return 'You win! Rock beats Scissors!';
     } else if (computerSelection=='rock' && playerSelection=='scissors'){
-        return 'Computer wins';
+        return 'Computer wins! Rock beats scissors!';
     } else if (computerSelection=='paper' && playerSelection=='rock'){
-        return 'Computer wins';
+        return 'Computer wins! Paper beats rock!';
     } else if (computerSelection=='scissors' && playerSelection=='paper'){
-        return 'Computer wins';                                                                                 
+        return 'Computer wins! Scissors beats paper!';                                                                                 
+} //Return 'You Win' vs. The Computer win based on that hierarchy.
 }
-}
-console.log (computerSelection);
-console.log (playerSelection);
-console.log (playRound());
 
-//Establish hierarchy of rock, paper, and scissors
-//Return 'You Win' vs. The Computer win based on that hierarchy.
+console.log (playRound(computerSelection,playerSelection));
+

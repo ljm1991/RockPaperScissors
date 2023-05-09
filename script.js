39597b80d47734959f1393631f8639866c30aee1
Lacy <lacy.mroz91@gmail.com>
2023-05-09 19:04:33 -0400

    Add html elements for rps choices and styling

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
-function getComputerChoice(){//get random computer choice
-    let computerChoice=Math.random();//generates a random number between 1 and 0
-    if (computerChoice > 0 && computerChoice < .333){ //defines random numbers into rock, paper, scissor choices
+function getComputerChoice() {//get random computer choice
+    let computerChoice = Math.random();//generates a random number between 1 and 0
+    if (computerChoice > 0 && computerChoice < .333) { //defines random numbers into rock, paper, scissor choices
         return 'rock';
-    } else if (computerChoice > .333 && computerChoice < .666){
+    } else if (computerChoice > .333 && computerChoice < .666) {
         return 'paper';
     } else {
         return 'scissors';
     }
 }
-function getPlayerChoice() {
-    return prompt('Rock, Paper, or Scissors?').toLowerCase();
-}
-//get player selection
-let playerScore=0;
-let computerScore=0;
+// function getPlayerChoice() {
+//     return prompt('Rock, Paper, or Scissors?').toLowerCase();
+//     }
 
-function playRound(computerSelection, playerSelection){ //Establish hierarchy of rock, paper, and scissors
-    if (computerSelection === playerSelection){
+//get player selection
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(computerSelection, playerSelection) { //Establish hierarchy of rock, paper, and scissors
+    if (computerSelection === playerSelection) {
         return 'Tie';
     }
-    else if (computerSelection=='rock' && playerSelection=='paper'){
+    else if (computerSelection == 'rock' && playerSelection == 'paper') {
         playerScore++;
         return 'You win the round! Paper beats rock!';
-    } else if (computerSelection == 'paper' && playerSelection=='scissors'){
+    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
         playerScore++;
         return 'You win the round! Scissors beats paper!';
-    } else if (computerSelection == 'scissors' && playerSelection=='rock'){
+    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
         playerScore++;
         return 'You win the round! Rock beats Scissors!';
-    } else if (computerSelection=='rock' && playerSelection=='scissors'){
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         computerScore++;
         return 'Computer wins the round! Rock beats scissors!';
-    } else if (computerSelection=='paper' && playerSelection=='rock'){
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         computerScore++;
         return 'Computer wins the round! Paper beats rock!';
-    } else if (computerSelection=='scissors' && playerSelection=='paper'){
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         computerScore++;
-        return 'Computer wins the round! Scissors beats paper!';                                                                                 
-} //Return 'You Win' vs. The Computer win based on that hierarchy.
+        return 'Computer wins the round! Scissors beats paper!';
+    } //Return 'You Win' vs. The Computer win based on that hierarchy.
 }
 
-function game(){//play game
-    for (let i =0; i <5; i++){//loop
-    let playerSelection= getPlayerChoice(); 
-    let computerSelection= getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-}
+function game() {//play game
+    for (let i = 0; i < 5; i++) {//loop
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
     console.log(finalScore(playerScore, computerScore));
 }
 
 function finalScore(playerScore, computerScore) {//add scoring
-    if (playerScore === computerScore){
+    if (playerScore === computerScore) {
         return `The game ends in a tie. ${playerScore} to ${computerScore}`;
-    } else if (playerScore > computerScore){
+    } else if (playerScore > computerScore) {
         return `Amazing! You beat the computer ${playerScore} to ${computerScore}.`;//calculate and return score
-    }else {
+    } else {
         return `Ouch. You lost to the computer ${computerScore} to ${playerScore}.`;
     }
 }

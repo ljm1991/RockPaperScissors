8670151f2963464335db032f745239f494fa5a2e
Lacy <lacy.mroz91@gmail.com>
2023-05-10 00:07:41 -0400

    Create and append elements with JS, add rounds and scoring

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const buttons = document.querySelectorAll("button")
-const score = document.querySelector("h3")
+const roundScore = document.createElement("h3")
+const container = document.querySelector("#container")
+const results = document.querySelector("#results")
+const scoreBoard = document.createElement("h4")
+scoreBoard.innerText = 'Current score is Player:0 to Computer:0'
+results.appendChild(scoreBoard);
+
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerChoice = `${button.id}`
-        console.log(playerChoice)
-        computerChoice = getComputerChoice();
+        computerChoice = getComputerChoice()
         playRound(playerChoice, computerChoice)
     })
 })
 
 function getComputerChoice() {
-    let randomInt = Math.floor(Math.random() * 1000) + 1
-    console.log(randomInt)
-    if (randomInt <= 333) {
+    let randomInt = Math.floor(Math.random() * 100) + 1
+    if (randomInt <= 33) {
         computerChoice = "rock"
     }
-    else if (randomInt > 333 && randomInt <= 666) {
+    else if (randomInt > 33 && randomInt <= 66) {
         computerChoice = "paper"
     } else {
         computerChoice = "scissors"
@@ -26,68 +34,41 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) { //Establish hierarchy of rock, paper, and scissors
     if (computerChoice === playerChoice) {
-        score.innerText = 'Tie'
+        roundScore.innerText = 'Tie'
     }
     else if (computerChoice == 'rock' && playerChoice == 'paper') {
-        score.innerText = 'You win the round! Paper beats rock!';
+        roundScore.innerText = 'You win the round! Paper beats rock!';
+        playerScore++
     } else if (computerChoice == 'paper' && playerChoice == 'scissors') {
-        score.innerText = 'You win the round! Scissors beats paper!';
+        roundScore.innerText = 'You win the round! Scissors beats paper!';
+        playerScore++
     } else if (computerChoice == 'scissors' && playerChoice == 'rock') {
-        score.innerText = 'You win the round! Rock beats Scissors!';
+        roundScore.innerText = 'You win the round! Rock beats Scissors!';
+        playerScore++
     } else if (computerChoice == 'rock' && playerChoice == 'scissors') {
-        score.innerText = 'Computer wins the round! Rock beats scissors!';
+        roundScore.innerText = 'Computer wins the round! Rock beats scissors!';
+        computerScore++
     } else if (computerChoice == 'paper' && playerChoice == 'rock') {
-        score.innerText = 'Computer wins the round! Paper beats rock!';
+        roundScore.innerText = 'Computer wins the round! Paper beats rock!';
+        computerScore++
     } else if (computerChoice == 'scissors' && playerChoice == 'paper') {
-        score.innerText = 'Computer wins the round! Scissors beats paper!';
+        roundScore.innerText = 'Computer wins the round! Scissors beats paper!';
+        computerScore++
+    }
+    results.innerText = `Current score is Player:${playerScore} to Computer:${computerScore}`
+    results.appendChild(roundScore)
+    if (computerScore === 5 || playerScore === 5) {
+        gameWin(playerScore, computerScore)
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
 
-function playRound(computerSelection, playerSelection) { //Establish hierarchy of rock, paper, and scissors
-    if (computerSelection === playerSelection) {
-        return 'Tie';
+function gameWin(playerScore, computerScore) {//add scoring
+    if (playerScore > computerScore) {
+        roundScore.innerText = `Amazing! You beat the computer ${playerScore} to ${computerScore}.`;//calculate and return score
+    } else {
+        roundScore.innerText = `Ouch. You lost to the computer ${computerScore} to ${playerScore}.`;
     }
-    else if (computerSelection == 'rock' && playerSelection == 'paper') {
-        playerScore++;
-        return 'You win the round! Paper beats rock!';
-    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
-        playerScore++;
-        return 'You win the round! Scissors beats paper!';
-    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
-        playerScore++;
-        return 'You win the round! Rock beats Scissors!';
-    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
-        computerScore++;
-        return 'Computer wins the round! Rock beats scissors!';
-    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
-        computerScore++;
-        return 'Computer wins the round! Paper beats rock!';
-    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
-        computerScore++;
-        return 'Computer wins the round! Scissors beats paper!';
-    } //Return 'You Win' vs. The Computer win based on that hierarchy.
 }
 
-// function game() {//play game
-//     for (let i = 0; i < 5; i++) {//loop
-//         let playerSelection = getPlayerChoice();
-//         let computerSelection = getComputerChoice();
-//         console.log(playRound(playerSelection, computerSelection));
-//     }
-//     console.log(finalScore(playerScore, computerScore));
-// }
 
-// function finalScore(playerScore, computerScore) {//add scoring
-//     if (playerScore === computerScore) {
-//         return `The game ends in a tie. ${playerScore} to ${computerScore}`;
-//     } else if (playerScore > computerScore) {
-//         return `Amazing! You beat the computer ${playerScore} to ${computerScore}.`;//calculate and return score
-//     } else {
-//         return `Ouch. You lost to the computer ${computerScore} to ${playerScore}.`;
-//     }
-// }
-
-game();
